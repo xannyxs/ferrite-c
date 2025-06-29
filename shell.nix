72fc7db9c3a5c9ebfca59ffb313d1e6450e8b4c1
Xannyx <56308735+xannyxs@users.noreply.github.com>
2025-06-29 19:10:31 +0200

    feat: Introduce robust debugging with Bochs and improve build system (#3)
    * Added debugging capabilities
    
    * Added DEBUG_H
    
    * Updated Bochs to 3.0
    
    * Added printing to the terminal

--- shell.nix
+++ shell.nix
@@ -5,6 +5,38 @@ let
     import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz")
       { };
 
+  bochs-src = pinnedPkgs.fetchurl {
+    url = "https://downloads.sourceforge.net/project/bochs/bochs/3.0/bochs-3.0.tar.gz";
+    sha256 = "sha256-y29UK1HzWizJIGsqmA21YCt80bfPLk7U8Ras1VB3gao=";
+  };
+
+  customBochs = pinnedPkgs.bochs.overrideAttrs (oldAttrs: {
+    name = "bochs-with-debugger";
+    src = bochs-src;
+
+    configureFlags = oldAttrs.configureFlags ++ [
+      "--enable-debugger"
+      "--enable-smp"
+      "--enable-cpu-level=6"
+      "--enable-all-optimizations"
+      "--enable-x86-64"
+      "--enable-pci"
+      "--enable-vmx"
+      "--enable-debugger-gui"
+      "--enable-logging"
+      "--enable-fpu"
+      "--enable-3dnow"
+      "--enable-sb16=dummy"
+      "--enable-cdrom"
+      "--enable-x86-debugger"
+      "--enable-iodebug"
+      "--disable-plugins"
+      "--disable-docbook"
+      "--with-x11"
+      "--with-term"
+    ];
+  });
+
   crossToolchain = pinnedPkgs.pkgsCross.i686-embedded.buildPackages.gcc;
 
 in
@@ -17,21 +49,16 @@ pinnedPkgs.mkShell {
     binutils
     xorriso
     clang-tools
-
     pkg-config
     mdbook
-
-    # Formatters
-    markdownlint-cli
-    alejandra
-
-    # Other
     grub2
     bear
     tree
+
+    customBochs
   ];
 
   shellHook = ''
-    echo "✅ Development environment from nixpkgs 23.11 is ready!"
+    echo "✅ Development environment with custom-built Bochs is ready!"
   '';
 }
