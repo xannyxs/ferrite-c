645e9914d27e102eb5a3c5c7c8d923d17cbabfdb
xannyx <xander@xvproductions.com>
2025-08-15 09:32:00 +0200

    Added tss gate in the gdt

--- src/arch/x86/gdt/gdt.c
+++ src/arch/x86/gdt/gdt.c
@@ -1,10 +1,14 @@
+#include "arch/x86/gdt/gdt.h"
 #include "arch/x86/entry.h"
 
 #include <stdint.h>
+#include <string.h>
 
 extern void gdt_flush(uint32_t);
+extern void *stack_top;
 
-entry_t gdt_entries[5] __attribute__((section(".gdt")));
+tss_entry_t tss_entry;
+entry_t gdt_entries[6] __attribute__((section(".gdt")));
 descriptor_pointer_t gdt_ptr;
 
 /* Private */
@@ -22,10 +26,21 @@ static void gdt_set_gate(uint32_t num, uint32_t base, uint32_t limit,
   gdt_entries[num].access = access;
 }
 
+static void tss_init(void) {
+  uint32_t base = (uint32_t)&tss_entry;
+  uint32_t limit = base + sizeof(tss_entry) - 1;
+
+  gdt_set_gate(5, base, limit, 0x89, 0x00);
+  memset(&tss_entry, 0, sizeof(tss_entry_t));
+
+  tss_entry.ss0 = 0x10;
+  tss_entry.esp0 = (uintptr_t)&stack_top;
+}
+
 /* Public */
 
 void gdt_init(void) {
-  gdt_ptr.limit = sizeof(entry_t) * 5 - 1;
+  gdt_ptr.limit = sizeof(entry_t) * 6 - 1;
   gdt_ptr.base = (uint32_t)&gdt_entries;
 
   gdt_set_gate(0, 0, 0, 0, 0);                // NULL Gate
@@ -34,5 +49,8 @@ void gdt_init(void) {
   gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User Code Segment
   gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User Data Segment
 
+  tss_init();
+
   gdt_flush((uint32_t)&gdt_ptr);
+  __asm__ volatile("ltr %0" : : "r"((uint16_t)0x28));
 }
--- src/arch/x86/gdt/gdt.h
+++ src/arch/x86/gdt/gdt.h
@@ -1,6 +1,24 @@
 #ifndef GDT_H
 #define GDT_H
 
+#include <stdint.h>
+
+typedef struct {
+  uint32_t prev_tss;
+  uint32_t esp0;
+
+  uint32_t ss0;
+  uint32_t esp1;
+  uint32_t ss1;
+  uint32_t esp2;
+  uint32_t ss2;
+
+  uint32_t cr3, eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  uint32_t es, cs, ss, ds, fs, gs;
+  uint32_t ldt;
+  uint16_t trap, iomap_base;
+} __attribute__((packed)) tss_entry_t;
+
 void gdt_init(void);
 
 #endif /* GDT_H */
