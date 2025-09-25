3c57bce4cc9b4486d056f0d188d0e86f7514ecf6
xannyx <xander@xvproductions.com>
2025-09-25 16:00:05 +0200

    Added basics of signals

---
+++ src/sys/signal/signal.c
@@ -0,0 +1,162 @@
+#include "sys/signal/signal.h"
+#include "drivers/printk.h"
+#include "sys/process.h"
+
+#include <stdint.h>
+
+extern proc_t ptables[NUM_PROC];
+extern proc_t *current_proc;
+
+sigaction_t sigaction[NSIG] = {
+    [SIGHUP] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGINT] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGQUIT] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGILL] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGTRAP] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGABRT] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGUNUSED] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGFPE] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+
+    [SIGKILL] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGUSR1] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGSEGV] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGUSR2] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGPIPE] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGALRM] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGTERM] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGSTKFLT] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+
+    [SIGCHLD] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGCONT] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGSTOP] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGTSTP] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGTTIN] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGTTOU] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGIO] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+
+    [SIGXCPU] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGXFSZ] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGVTALRM] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGPROF] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+    [SIGWINCH] = {.sa_handler = SIG_IGN, .sa_flags = 0},
+    [SIGPWR] = {.sa_handler = SIG_DFL, .sa_flags = 0},
+};
+
+/* Private */
+
+static inline void __default_sigterm_handler(int32_t sig) {
+  printk("Process %d terminated by signal %d\n", current_proc->pid, sig);
+  current_proc->state = ZOMBIE;
+  yield();
+}
+
+static inline void __default_sigkill_handler(int32_t sig) {
+  printk("Process %d killed by signal %d\n", current_proc->pid, sig);
+  current_proc->state = ZOMBIE;
+  yield();
+}
+
+static inline void __default_sigcore_handler(int32_t sig) {
+  printk("Process %d core dumped by signal %d\n", current_proc->pid, sig);
+  // TODO: Dump core
+  current_proc->state = ZOMBIE;
+  yield();
+}
+
+static inline void __default_sigstop_handler(int32_t sig) {
+  printk("Process %d stopped by signal %d\n", current_proc->pid, sig);
+  current_proc->state = SLEEPING;
+  yield();
+}
+
+/* Public */
+
+int32_t do_kill(pid_t pid, int32_t sig) {
+  if (sig < 1 || sig > NSIG) {
+    return -1;
+  }
+
+  proc_t *p = find_process(pid);
+  if (!p) {
+    return -1;
+  }
+
+  p->pending_signals = (1 << sig);
+
+  if (p->state == SLEEPING) {
+    p->state = READY;
+  }
+
+  printk("Process %d sent signal %d to process %d\n", current_proc->pid, sig,
+         pid);
+
+  return 0;
+}
+
+void handle_signal(void) {
+  if (!current_proc->pending_signals) {
+    return;
+  }
+
+  for (int32_t sig = 1; sig < NSIG; sig += 1) {
+    if (!(current_proc->pending_signals & (1 << sig))) {
+      continue;
+    }
+
+    current_proc->pending_signals &= ~(1 << sig);
+    __sighandler_t handler = sigaction[sig].sa_handler;
+    if (handler == SIG_IGN) {
+      continue;
+    }
+
+    if (!handler && handler == SIG_DFL) {
+      switch (sig) {
+      case SIGSTOP:
+      case SIGTSTP:
+      case SIGTTIN:
+      case SIGTTOU:
+        __default_sigstop_handler(sig);
+        break;
+
+      case SIGKILL:
+      case SIGTERM:
+      case SIGINT:
+        __default_sigkill_handler(sig);
+        break;
+
+      case SIGCONT:
+        if (current_proc->state == SLEEPING) {
+          current_proc->state = READY;
+        }
+        break;
+
+      case SIGILL:
+      case SIGTRAP:
+      case SIGABRT:
+      case SIGFPE:
+      case SIGSEGV:
+      case SIGBUS:
+        __default_sigcore_handler(sig);
+        break;
+
+      case SIGPIPE:
+      case SIGALRM:
+      case SIGUSR2:
+      case SIGHUP:
+      case SIGQUIT:
+        __default_sigterm_handler(sig);
+        break;
+
+      default:
+        // Unknown signal
+        printk("Process %d: unknown signal %d, ignoring\n", current_proc->pid,
+               sig);
+        break;
+      }
+    }
+
+    printk("Process %d handling signal %d with custom handler\n",
+           current_proc->pid, sig);
+    handler(sig);
+  }
+}
---
+++ src/sys/signal/signal.h
@@ -0,0 +1,70 @@
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+#include "sys/process.h"
+
+#include <stdint.h>
+
+#define _NSIG 32
+#define NSIG _NSIG
+
+#define SIGHUP 1
+#define SIGINT 2
+#define SIGQUIT 3
+#define SIGILL 4
+#define SIGTRAP 5
+#define SIGABRT 6
+#define SIGIOT 6
+#define SIGUNUSED 7
+#define SIGFPE 8
+#define SIGKILL 9
+#define SIGUSR1 10
+#define SIGSEGV 11
+#define SIGUSR2 12
+#define SIGPIPE 13
+#define SIGALRM 14
+#define SIGTERM 15
+#define SIGSTKFLT 16
+#define SIGCHLD 17
+#define SIGCONT 18
+#define SIGSTOP 19
+#define SIGTSTP 20
+#define SIGTTIN 21
+#define SIGTTOU 22
+
+#define SIGIO 23
+#define SIGPOLL SIGIO
+#define SIGURG SIGIO
+#define SIGXCPU 24
+#define SIGXFSZ 25
+
+#define SIGVTALRM 26
+#define SIGPROF 27
+
+#define SIGWINCH 28
+
+// #define SIGLOST		29
+#define SIGPWR 30
+
+/* Arggh. Bad user source code wants this.. */
+#define SIGBUS SIGUNUSED
+
+typedef uint32_t sigset_t;
+typedef void (*__sighandler_t)(int32_t);
+
+#define SIG_DFL ((__sighandler_t)0)    /* default signal handling */
+#define SIG_IGN ((__sighandler_t)1)    /* ignore signal */
+#define SIG_ERR ((__sighandler_t) - 1) /* error return from signal */
+
+typedef struct {
+  __sighandler_t sa_handler;
+  sigset_t sa_mask;
+  int32_t sa_flags;
+  void (*sa_restorer)(void);
+} sigaction_t;
+
+int32_t do_kill(pid_t pid, int32_t sig);
+
+void handle_signal(void);
+
+#endif /* SIGNAL_H */
