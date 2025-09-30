f8ce8f63cecbabfa6de974a49b7a21f6945b2358
xannyx <xander@xvproductions.com>
2025-09-30 17:17:35 +0200

    Minor fix

--- src/sys/shell.c
+++ src/sys/shell.c
@@ -9,17 +9,14 @@ extern tty_t tty;
 extern proc_t ptables[NUM_PROC];
 
 void process_list(void) {
-  printk("PID  STATE  NAME  PPID  PARENT ADDRESS\n");
-  printk("---  -----  ----  ----  --------------\n");
-
+  printk("PID  STATE     NAME              PPID  PARENT ADDRESS\n");
+  printk("---  --------  ----------------  ----  --------------\n");
   for (int32_t i = 0; i < NUM_PROC; i += 1) {
     if (ptables[i].state != UNUSED) {
       const char *state_str[] = {"UNUSED", "EMBRYO",  "SLEEPING",
                                  "READY",  "RUNNING", "ZOMBIE"};
-
       pid_t ppid = ptables[i].parent ? ptables[i].parent->pid : 0;
-
-      printk("%d  %s  %s  %d  0x%x\n", ptables[i].pid,
+      printk("%3d  %8s  %16s  %4d  0x%08x\n", ptables[i].pid,
              state_str[ptables[i].state], ptables[i].name, ppid,
              ptables[i].parent);
     }
