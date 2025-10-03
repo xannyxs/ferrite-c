379d7346a5ceb258802ca3a553a7883704578685
xannyx <xander@xvproductions.com>
2025-10-03 12:57:01 +0200

    Added on-demand paging

--- src/arch/x86/idt/exceptions.c
+++ src/arch/x86/idt/exceptions.c
@@ -1,10 +1,17 @@
 #include "arch/x86/idt/idt.h"
 #include "debug/debug.h"
 #include "debug/panic.h"
+#include "drivers/printk.h"
+#include "memory/vmm.h"
+#include "sys/process.h"
 #include "types.h"
 
 #define USER_SPACE 1
 #define KERNEL_SPACE 3
+
+// TODO: Might need to move this
+#define USER_SPACE_START 0x00000000
+#define USER_SPACE_END 0xC0000000
 
 __attribute__((target("general-regs-only"), interrupt)) void
 divide_by_zero_handler(registers_t* regs)
@@ -130,26 +137,40 @@ general_protection_fault(registers_t* regs, u32 error_code)
 {
     (void)error_code;
     if ((regs->cs & 3) == 0) {
+        printk("GPF: error=%x, cs=%x\n",
+            error_code, regs->cs);
         panic(regs, "General Protection Fault");
     }
 
+    printk("GPF: error=%x, cs=%x\n",
+        error_code, regs->cs);
     __asm__ volatile("cli; hlt");
 }
 
 __attribute__((target("general-regs-only"), interrupt)) void
 page_fault(registers_t* regs, u32 error_code)
 {
-    (void)error_code;
-    if ((regs->cs & KERNEL_SPACE) == 0) {
-        panic(regs, "Page Fault");
+    u32 fault_addr;
+    __asm__ volatile("movl %%cr2, %0" : "=r"(fault_addr));
+
+    if ((regs->cs & 0x3) == 0x3) {
+        proc_t* p = myproc();
+
+        if (fault_addr < USER_SPACE_END) {
+            if ((error_code & PTE_P) == 0) {
+                void* page_base = (void*)(fault_addr & ~0xFFF);
+
+                if (vmm_map_page(NULL, page_base, PTE_P | PTE_W | PTE_U) == 0) {
+                    printk("On-demand mapped page at %p for PID %d\n",
+                        page_base, p->pid);
+                    return;
+                }
+            }
+        }
     }
 
-    /* TODO:
-     * Implement On-Demand Paging (lazy loading) for the user space.
-     *
-     * To make the kernel more effecient, we do not map anything in the user
-     * space, until there is a page fault. It will then ensure the physical
-     * address is being mapped with a virtual address.
-     */
-    __asm__ volatile("cli; hlt");
+    printk("Page Fault: addr=%p, error=%x, cs=%x\n",
+        fault_addr, error_code, regs->cs);
+    panic(regs, "Page Fault");
+    __builtin_unreachable();
 }
