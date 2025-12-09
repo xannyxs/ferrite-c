5ada2881dcaa24808222202647a96a36c62ceae1
Xannyx <56308735+xannyxs@users.noreply.github.com>
2025-12-09 16:52:19 +0100

    Added a prototype of execve, but I will come back to it in kfs-9 (#56)

---
+++ src/fs/exec.h
@@ -0,0 +1,44 @@
+#ifndef _BIN_PROGRAMS_H
+#define _BIN_PROGRAMS_H
+
+#include "arch/x86/idt/idt.h"
+#include "fs/vfs.h"
+
+/*
+ * From Linux 1.0:
+ * https://elixir.bootlin.com/linux/1.0.9/source/include/linux/binfmts.h#L16
+ *
+ * MAX_ARG_PAGES defines the number of pages allocated for arguments
+ * and envelope for the new program. 32 should suffice, this gives
+ * a maximum env+arg of 128kB !
+ */
+#define MAX_ARG_PAGES 32
+
+/*
+ * This structure is used to hold the arguments that are used when loading
+ * binaries.
+ */
+typedef struct binpgm {
+    char b_buf[128];
+
+    unsigned long b_page[MAX_ARG_PAGES];
+    unsigned long b_p;
+
+    vfs_inode_t* b_node;
+
+    int e_uid, e_gid;
+    int argc, envc;
+
+    int b_sh_bang;
+    char* b_filename; /* Name of binary */
+} binpgm_t;
+
+typedef struct binfmt {
+    int (*load_binary)(binpgm_t*, registers_t*);
+    int (*load_shlib)(int);
+} binfmt_t;
+
+/* exec.c */
+int do_execve(char const*, char const* const*, char const* const*);
+
+#endif
