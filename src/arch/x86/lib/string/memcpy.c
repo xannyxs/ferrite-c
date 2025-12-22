412e6d329b2d41f3f0503b497eb958d8efa78b79
xannyx <xander@xvproductions.com>
2025-12-22 15:38:48 +0100

    optimized memcpy

--- src/arch/x86/lib/string/memcpy.c
+++ src/arch/x86/lib/string/memcpy.c
@@ -3,15 +3,29 @@
 
 void* memcpy(void* dest, void const* src, size_t n)
 {
-    u8* d = dest;
-    u8 const* s = src;
+    int d0, d1, d2;
 
-    while (n) {
-        n--;
-        *d = *s;
-        d++;
-        s++;
+    if (n == 0 || src == dest) {
+        return dest;
     }
 
-    return (dest);
+    __asm__ volatile("cld\n\t"
+                     "movl %%edx, %%ecx\n\t"
+                     "shrl $2, %%ecx\n\t"
+                     "rep; movsl\n\t"
+                     "testb $1, %%dl\n\t"
+                     "je 1f\n\t"
+
+                     "movsb\n"
+                     "1:\ttestb $2,%%dl\n\t"
+                     "je 2f\n\t"
+                     "movsw\n"
+
+                     "2:\n"
+
+                     : "=&c"(d0), "=&D"(d1), "=&S"(d2)
+                     : "d"(n), "1"(dest), "2"(src)
+                     : "memory");
+
+    return dest;
 }
