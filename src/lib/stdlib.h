93e80db6a04f64aebf7bfeb541cae576ba9a7bb6
xannyx <xander@xvproductions.com>
2026-01-05 15:52:08 +0100

    Added atol into the library

--- src/lib/stdlib.h
+++ src/lib/stdlib.h
@@ -3,4 +3,10 @@
 
 __attribute__((__noreturn__)) void abort(char*);
 
+/*
+ * WARNING: 'atol' used to convert a string to an integer value, but function
+ * will not report conversion errors; consider using 'strtol' instead
+ */
+long atol(char const*);
+
 #endif
