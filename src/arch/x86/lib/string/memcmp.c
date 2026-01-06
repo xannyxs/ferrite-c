569bbf4f5ebb9ad6d16b8c500f6b3037ef6172b9
xannyx <xander@xvproductions.com>
2026-01-06 20:25:38 +0100

    Added memcmp

---
+++ src/arch/x86/lib/string/memcmp.c
@@ -0,0 +1,21 @@
+#include <ferrite/types.h>
+
+int memcmp(void const* _l, void const* _r, size_t len)
+{
+    int d0, d1, d2;
+    int __res;
+
+    __asm__ volatile("cld\n\t"
+                     "repe\n\t"
+                     "cmpsb\n\t"
+                     "je 1f\n\t"
+                     "movl $1,%%eax\n\t"
+                     "jb 1f\n\t"
+                     "negl %%eax\n"
+                     "1:"
+
+                     : "=a"(__res), "=&c"(d0), "=&D"(d1), "=&S"(d2)
+                     : "0"(0), "2"(_l), "3"(_r), "1"(len)
+                     : "memory");
+    return __res;
+}
