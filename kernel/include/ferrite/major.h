0a7afe29cc080cf3e8a8b0469145f39a86905852
xannyx <xander@xvproductions.com>
2026-01-12 11:38:23 +0100

    Added new MAJOR segments

--- kernel/include/ferrite/major.h
+++ kernel/include/ferrite/major.h
@@ -1,6 +1,48 @@
 #ifndef _FERRITE_MAJOR_H
 #define _FERRITE_MAJOR_H
 
+#define MAX_CHRDEV 32
+
+/*
+ * From Linux 1.0:
+ * https://elixir.bootlin.com/linux/1.0.9/source/include/linux/major.h#L10
+ *
+ * assignments
+ *
+ * devices are as follows (same as minix, so we can use the minix fs):
+ *
+ *      character              block                  comments
+ *      --------------------   --------------------   --------------------
+ *  0 - unnamed                unnamed                minor 0 = true nodev
+ *  1 - /dev/mem               ramdisk
+ *  2 -                        floppy
+ *  3 -                        hd
+ *  4 - /dev/tty*
+ *  5 - /dev/tty; /dev/cua*
+ *  6 - lp
+ *  7 -                                               UNUSED
+ *  8 -                        scsi disk
+ *  9 - scsi tape
+ * 10 - mice
+ * 11 -                        scsi cdrom
+ * 12 - qic02 tape
+ * 13 -                        xt disk
+ * 14 - sound card
+ * 15 -                        cdu31a cdrom
+ * 16 - sockets
+ * 17 - af_unix
+ * 18 - af_inet
+ * 19 -                                               UNUSED
+ * 20 -                                               UNUSED
+ * 21 - scsi generic
+ * 22 -                        (at2disk)
+ * 23 -                        mitsumi cdrom
+ * 24 -	                       sony535 cdrom
+ * 25 -                        matsushita cdrom       minors 0..3
+ * 26 -
+ * 27 - qic117 tape
+ */
+
 #define UNNAMED_MAJOR 0
 #define IDE0_MAJOR 3
 #define IDE1_MAJOR 22
